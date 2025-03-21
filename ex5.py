b1db8229c9f85cf1d9faec026753811628341eb0
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-03-21 10:56:48 -0300

    Delete ex5.py

--- ex5.py
+++
@@ -1,7 +0,0 @@
-copias = int(input('Insira o número de cópias desejadas:'))
-if copias < 100:
-    formula = copias * 0.25
-    print('Valor total:', formula, 'reais')
-else:
-    formula2 = copias * 0.20
-    print('Valor total:', formula2,'reais')
