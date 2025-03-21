085f42bc810c845e60d4be4a43f6bcb6b03bca09
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-03-21 10:56:07 -0300

    Delete ex2.py

--- ex2.py
+++
@@ -1,7 +0,0 @@
-nascimento = int(input('Insira seu ano de nascimento:'))
-idade = 2023 - nascimento
-print('Você tem', idade, 'anos')
-if idade >= 18:
-    print('Você pode fazer a carteira de motorista')
-else:
-    print('Você não pode fazer a carteira de motorista')
