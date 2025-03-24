4987a59f94c934a1f3e50b0e0296ddd2746573b0
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-03-24 10:36:41 -0300

    Update and rename ex7 - revisar.py to Exercício 7 - Revisar.py

--- TDE 2 - Resolução da Lista de Exercícios 2/Exercício 7 - Revisar.py
+++ TDE 2 - Resolução da Lista de Exercícios 2/Exercício 7 - Revisar.py
@@ -1,8 +1,9 @@
 peso = float(input('Insira se peso em quilogramas:'))
 altura = float(input('Insira sua altura em metros:'))
 formula = peso / altura**2
+formula2 = 24.9 * altura**2
 print('Seu imc é:', formula)
 if formula >= 18.5 and formula <= 25:
     print('Seu imc é considerado normal')
 else:
-    print('Sua massa considerada ideal é:')
+    print('Seu peso deveria ser de:', formula2, ' para seu imc ser considerado ideal')
