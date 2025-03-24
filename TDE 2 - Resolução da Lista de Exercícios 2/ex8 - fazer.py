c3753722d362f49f0f401b620ae062fdb6fd8a7a
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-03-24 11:00:06 -0300

    Update ex8 - fazer.py

--- TDE 2 - Resolução da Lista de Exercícios 2/ex8 - fazer.py
+++ TDE 2 - Resolução da Lista de Exercícios 2/ex8 - fazer.py
@@ -1,3 +1,12 @@
-tempo = int(input('Quanto tempo em minutos você passou no estacionamento?'))
+tempo = float(input('Insira o tempo em minutos que você ficou no estacionamento:'))
+tempo2 = tempo - 60
+preco = 8
 if tempo == 60:
-    print('Valor total: 8 reias')
+    print('O valor a ser pago é:', preco, 'reais')
+elif tempo > 60:
+    print('O valor a ser pago é:', tempo + 1.5 + 8, 'reais')
+
+
+
+
+
