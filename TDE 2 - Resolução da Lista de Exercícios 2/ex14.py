82fd4d4f722fad84d266b9af273051b0ebdd1c87
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-03-24 09:51:00 -0300

    Update ex14.py

--- TDE 2 - Resolução da Lista de Exercícios 2/ex14.py
+++ TDE 2 - Resolução da Lista de Exercícios 2/ex14.py
@@ -1,3 +1,10 @@
-numero = int(input('Insira um número inteiro:'))
-numero2 = int(input('Insira outro número inteiro:'))
-numero3 = int(input('Insira mais um número inteiro:'))
+a = int(input('Insira um número inteiro:'))
+b = int(input('Insira outro número inteiro:'))
+c = int(input('Insira mais um número inteiro'))
+if a > b and a > c:
+    print('O maior número é:', a)
+elif b > a and b > c:
+    print('O maior número é:', b)
+elif c > a and c > b:
+    print('O maior número é:', c)
+
