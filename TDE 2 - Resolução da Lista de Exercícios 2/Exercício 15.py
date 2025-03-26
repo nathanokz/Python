d8bdc7a458e78335b0527c42a9422a475da86916
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-03-26 20:10:43 -0300

    Update Exercício 15.py

--- TDE 2 - Resolução da Lista de Exercícios 2/Exercício 15.py
+++ TDE 2 - Resolução da Lista de Exercícios 2/Exercício 15.py
@@ -1,12 +1,21 @@
 a = int(input('Insira um número inteiro:'))
 b = int(input('Insira outro número inteiro:'))
 c = int(input('Insira mais um número inteiro'))
-if a < b and a < c or b < a and b < c:
-        print(c, b, a)
-elif b < a and b < c or c < a and c < b:
+if a >= b and a >= c:
+    if b >= c:
+        print(a, b, c)
+    else:
         print(a, c, b)
-
-
+elif b >= a and b >= c:
+    if a >= c:
+        print(b, a, c)
+    else:
+        print(b, c, a)
+else:
+    if a >= b:
+        print(c, a, b)
+    else:
+        print(c, b, a)
 
 
 
