38859b56b00b114808ff8438fc55375dc8dc4107
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-03-28 11:48:04 -0300

    Update Exercício 9.py

--- TDE 3 - Resolução da Lista de Exercícios 3/Exercício 9.py
+++ TDE 3 - Resolução da Lista de Exercícios 3/Exercício 9.py
@@ -1,31 +1,8 @@
-dolar = 5.76
-euro = 6.24
-libra = 7.43
-moedadesejada = 0
-while moedadesejada < 1 or moedadesejada > 3:
-    print('Escolha uma moeda:')
-    print('1 - Dólar')
-    print('2 - Euro')
-    print('3 - Libra')
-    moedadesejada = int(input('Insira a moeda desejada:'))
-montante = float(input('Digite a quantidade da moeda desejada:'))
-if moedadesejada == 1:
-    if montante < 1000:
-        print('Valor total:', montante * dolar * 1.05)
-    else:
-        print('Valor total:', montante * dolar * 1.03)
-elif moedadesejada == 2:
-    if montante < 1000:
-        print('Valor total:', montante * euro * 1.05 )
-    else:
-        print('Valor total:', montante * euro * 1.03)
-elif moedadesejada == 3:
-    if montante < 1000:
-        print('Valor total:', montante * libra * 1.05)
-    else:
-        print('valor total:', montante * libra * 1.03)
-
-
-
-
-
+li = float(input('Insira um limite incical:'))
+lf = float(input('Insira um limite final:'))
+inicio = li + 1 if (li + 1) % 3 != 0 else li + 1
+contador = inicio
+while contador < lf:
+    if contador % 3 == 0:
+        print('Número:', contador)
+    contador += 1
