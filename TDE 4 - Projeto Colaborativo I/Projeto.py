5b20ee908c92c8aacee2a647ebf01bf7e76b685e
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-04-03 08:33:25 -0300

    Update Projeto.py

--- TDE 4 - Projeto Colaborativo I/Projeto.py
+++ TDE 4 - Projeto Colaborativo I/Projeto.py
@@ -1,5 +1,5 @@
 entradas = int(input('1 - 2 Players , 2 - Player x Bot e 3 - Bot x Bot \nSelecione uma modalidade:'))
-
+print('--------------------------------------------------------------------------------------')
 if entradas == 1:
     print('Modalidade selecionada: Player x Player')
     print('Regras do jogo: Pedra ganha da tesoura / Tesoura ganha do papel / Papel ganha da pedra')
@@ -9,14 +9,16 @@ if entradas == 1:
 
     contador = 1
     while contador:
+        print('--------------------------------------------------------------------------------------')
         jogadas1 = int(input('Pedra - 1, Papel - 2 e Tesoura - 3 \nSelecione sua jogada:'))
         jogadas2 = int(input('Pedra - 1, Papel - 2 e Tesoura - 3 \nSelecione sua jogada:'))
-
+        print('--------------------------------------------------------------------------------------')
         if jogadas1 == 1 and jogadas2 == 2:
             print('Jogador 2 venceu!')
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer2 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nPlayer 2 =', placarplayer2, 'pontos')
                 break
 
@@ -25,6 +27,7 @@ if entradas == 1:
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer1 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nPlayer 2 =', placarplayer2, 'pontos')
                 break
 
@@ -33,6 +36,7 @@ if entradas == 1:
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer1 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nPlayer 2 =', placarplayer2, 'pontos')
                 break
 
@@ -41,6 +45,7 @@ if entradas == 1:
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer2 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nPlayer 2 =', placarplayer2, 'pontos')
                 break
 
@@ -49,6 +54,7 @@ if entradas == 1:
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer2 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nPlayer 2 =', placarplayer2, 'pontos')
                 break
 
@@ -57,13 +63,15 @@ if entradas == 1:
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer1 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nPlayer 2 =', placarplayer2, 'pontos')
                 break
     contador += 1
 
 elif entradas == 2:
     import random
-    numeros = random.randint(0, 3)
+
+    numeros = random.randint(1, 3)
     print('Modalidade selecionada: Player x Bot')
     print('Regras do jogo: Pedra ganha da tesoura / Tesoura ganha do papel / Papel ganha da pedra')
 
@@ -72,60 +80,73 @@ elif entradas == 2:
 
     contador = 1
     while contador:
+        print('--------------------------------------------------------------------------------------')
         jogadas1 = int(input('Pedra - 1, Papel - 2 e Tesoura - 3 \nSelecione sua jogada:'))
         jogadasbot = numeros
 
         if jogadas1 == 1 and jogadasbot == 2:
-            print('Bot escolheu:', numeros )
+            print('Bot escolheu:', numeros)
             print('Bot venceu!')
+            print('--------------------------------------------------------------------------------------')
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarbot += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nBot =', placarbot, 'pontos')
                 break
 
         if jogadas1 == 1 and jogadasbot == 3:
             print('Bot escolheu:', numeros)
             print('Jogador 1 venceu!')
+            print('--------------------------------------------------------------------------------------')
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer1 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nBot =', placarbot, 'pontos')
                 break
 
         if jogadas1 == 2 and jogadasbot == 1:
             print('Bot escolheu:', numeros)
             print('Jogador 1 venceu!')
+            print('--------------------------------------------------------------------------------------')
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer1 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nBot =', placarbot, 'pontos')
                 break
 
         if jogadas1 == 2 and jogadasbot == 3:
             print('Bot escolheu:', numeros)
             print('Bot venceu!')
+            print('--------------------------------------------------------------------------------------')
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarbot += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nBot =', placarbot, 'pontos')
                 break
 
         if jogadas1 == 3 and jogadasbot == 1:
             print('Bot escolheu:', numeros)
             print('Bot venceu!')
+            print('--------------------------------------------------------------------------------------')
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarbot += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nBot =', placarbot, 'pontos')
                 break
 
         if jogadas1 == 3 and jogadasbot == 2:
             print('Bot escolheu:', numeros)
             print('Jogador 1 venceu!')
+            print('--------------------------------------------------------------------------------------')
             opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
             placarplayer1 += 1
             if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
                 print('Player 1 =', placarplayer1, 'pontos', '\nBot =', placarbot, 'pontos')
                 break
     contador += 1
@@ -133,8 +154,10 @@ elif entradas == 2:
 
 elif entradas == 3:
     import random
+
     numeros1 = random.randint(0, 3)
     import random
+
     numeros2 = random.randint(0, 3)
     print('Modalidade selecionada: Bot x Bot')
     print('Regras do jogo: Pedra ganha da tesoura / Tesoura ganha do papel / Papel ganha da pedra')
@@ -144,70 +167,83 @@ elif entradas == 3:
 
     contador = 1
     while contador:
-            jogadasbot1 = numeros1
-            jogadasbot2 = numeros2
+        print('--------------------------------------------------------------------------------------')
+        jogadasbot1 = numeros1
+        jogadasbot2 = numeros2
 
-            if jogadasbot1 == 1 and jogadasbot2 == 2:
-                print('Bot 1 escolheu:', numeros1)
-                print('Bot 2 escolheu:', numeros2)
-                print('Bot 2 venceu!')
-                opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
-                placarbot2 += 1
-                if opcao == 2:
-                    print('Bot 1 =', placarbot1,'pontos', '\nBot 2 =', placarbot2, 'pontos')
-                    break
+        if jogadasbot1 == 1 and jogadasbot2 == 2:
+            print('Bot 1 escolheu:', numeros1)
+            print('Bot 2 escolheu:', numeros2)
+            print('Bot 2 venceu!')
+            print('--------------------------------------------------------------------------------------')
+            opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
+            placarbot2 += 1
+            if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
+                print('Bot 1 =', placarbot1, 'pontos', '\nBot 2 =', placarbot2, 'pontos')
+                break
 
-            if jogadasbot1 == 1 and jogadasbot2 == 3:
-                print('Bot 1 escolheu:', numeros1)
-                print('Bot 2 escolheu:', numeros2)
-                print('Bot 1 venceu!')
-                opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
-                placarbot1 += 1
-                if opcao == 2:
-                    print('Player 1 =', placarbot1, 'pontos', '\nBot =', placarbot2, 'pontos')
-                    break
+        if jogadasbot1 == 1 and jogadasbot2 == 3:
+            print('Bot 1 escolheu:', numeros1)
+            print('Bot 2 escolheu:', numeros2)
+            print('Bot 1 venceu!')
+            print('--------------------------------------------------------------------------------------')
+            opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
+            placarbot1 += 1
+            if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
+                print('Bot 1 =', placarbot1, 'pontos', '\nBot 2 =', placarbot2, 'pontos')
+                break
 
-            if jogadasbot1 == 2 and jogadasbot2 == 1:
-                print('Bot 1 escolheu:', numeros1)
-                print('Bot 2 escolheu:', numeros2)
-                print('Bot 1 venceu!')
-                opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
-                placarbot1 += 1
-                if opcao == 2:
-                    print('Player 1 =', placarbot1, 'pontos', '\nBot =', placarbot2, 'pontos')
-                    break
+        if jogadasbot1 == 2 and jogadasbot2 == 1:
+            print('Bot 1 escolheu:', numeros1)
+            print('Bot 2 escolheu:', numeros2)
+            print('Bot 1 venceu!')
+            print('--------------------------------------------------------------------------------------')
+            opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
+            placarbot1 += 1
+            if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
+                print('Bot 1 =', placarbot1, 'pontos', '\nBot 2 =', placarbot2, 'pontos')
+                break
 
-            if jogadasbot1 == 2 and jogadasbot2 == 3:
-                print('Bot 1 escolheu:', numeros1)
-                print('Bot 2 escolheu:', numeros2)
-                print('Bot 2 venceu!')
-                opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
-                placarbot2 += 1
-                if opcao == 2:
-                    print('Player 1 =', placarbot1, 'pontos', '\nBot =', placarbot2, 'pontos')
-                    break
-            
-            if jogadasbot1 == 3 and jogadasbot2 == 1:
-                print('Bot 1 escolheu:', numeros1)
-                print('Bot 2 escolheu:', numeros2)
-                print('Bot 2 venceu!')
-                opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
-                placarbot2 += 1
-                if opcao == 2:
-                    print('Player 1 =', placarbot1, 'pontos', '\nBot =', placarbot2, 'pontos')
-                    break
-            
-            if jogadasbot1 == 3 and jogadasbot2 == 2:
-                print('Bot 1 escolheu:', numeros1)
-                print('Bot 2 escolheu:', numeros2)
-                print('Jogador 1 venceu!')
-                opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
-                placarbot1 += 1
-                if opcao == 2:
-                    print('Player 1 =', placarbot1, 'pontos', '\nBot =', placarbot2, 'pontos')
-                    break
+        if jogadasbot1 == 2 and jogadasbot2 == 3:
+            print('Bot 1 escolheu:', numeros1)
+            print('Bot 2 escolheu:', numeros2)
+            print('Bot 2 venceu!')
+            print('--------------------------------------------------------------------------------------')
+            opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
+            placarbot2 += 1
+            if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
+                print('Bot 1 =', placarbot1, 'pontos', '\nBot 2 =', placarbot2, 'pontos')
+                break
+
+        if jogadasbot1 == 3 and jogadasbot2 == 1:
+            print('Bot 1 escolheu:', numeros1)
+            print('Bot 2 escolheu:', numeros2)
+            print('Bot 2 venceu!')
+            print('--------------------------------------------------------------------------------------')
+            opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
+            placarbot2 += 1
+            if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
+                print('Bot 1 =', placarbot1, 'pontos', '\nBot 2=', placarbot2, 'pontos')
+                break
+
+        if jogadasbot1 == 3 and jogadasbot2 == 2:
+            print('Bot 1 escolheu:', numeros1)
+            print('Bot 2 escolheu:', numeros2)
+            print('Bot 1 venceu!')
+            print('--------------------------------------------------------------------------------------')
+            opcao = int(input('Deseja continuar? 1 - SIM e 2 - NÃO \nSelecione a opção:'))
+            placarbot1 += 1
+            if opcao == 2:
+                print('--------------------------------------------------------------------------------------')
+                print('Bot 1 =', placarbot1, 'pontos', '\nBot 2=', placarbot2, 'pontos')
+                break
+
     contador += 1
-
 
 
 
