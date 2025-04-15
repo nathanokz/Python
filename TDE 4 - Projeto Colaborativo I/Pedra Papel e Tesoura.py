8d6a7eb92ff1a13dc1e9de1c53d899921dc5a51e
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-04-15 08:52:03 -0300

    Update Pedra Papel e Tesoura.py

--- TDE 4 - Projeto Colaborativo I/Pedra Papel e Tesoura.py
+++ TDE 4 - Projeto Colaborativo I/Pedra Papel e Tesoura.py
@@ -3,12 +3,14 @@ modalidade = int(input('Selecione uma modalidade: (1) 2 Players, (2) Player x Bo
 contador = 1
 placarjogada1 = 0
 placarjogada2 = 0
+placarempate = 0
 while contador == 1:
     if modalidade == 1:
         jogada1 = int(input('Selecione uma jogada: (1) Pedra, (2) Papel e (3) Tesoura'))
         jogada2 = int(input('Selecione uma jogada: (1) Pedra, (2) Papel e (3) Tesoura'))
         if jogada1 == jogada2:
             print('Empate!')
+            placarempate += 1
         elif (jogada1 == 1 and jogada2 == 3) or (jogada1 == 2 and jogada2 == 1) or (jogada1 == 3 and jogada2 == 2):
             print('Player 1 venceu!')
             placarjogada1 += 1
@@ -20,6 +22,7 @@ while contador == 1:
         jogadabot1 = random.randint(1, 3)
         if jogada1 == jogadabot1:
             print('Empate!')
+            placarempate += 1
         elif (jogada1 == 1 and jogadabot1 == 3) or (jogada1 == 2 and jogadabot1 == 1) or (jogada1 == 3 and jogadabot1 == 2):
             print('Player 1 venceu!')
             placarjogada1 += 1
@@ -31,6 +34,7 @@ while contador == 1:
         jogadabot2 = random.randint(1, 3)
         if jogadabot1 == jogadabot2:
             print('Empate!')
+            placarempate += 1
         elif (jogadabot1 == 1 and jogadabot2 == 3) or (jogadabot1 == 2 and jogadabot2 == 1) or (jogadabot1 == 3 and jogadabot2 == 2):
             print('O Bot 1 venceu!')
             placarjogada1 += 1
@@ -38,6 +42,15 @@ while contador == 1:
             print('O Bot 2 venceu!')
             placarjogada2 += 1
     continuar = input('Deseja continuar? (s) Sim e (n) Não')
-    if continuar == 'n':
-        print('Placar: Player 1 = {} pontos e Player 2 = {} pontos \nObrigado por jogar! Feito por: Heitor, Hugo, João e Nathan'.format(placarjogada1, placarjogada2))
-        break
+    if modalidade == 1:
+        if continuar == 'n':
+            print('Placar: Player 1 = {} pontos, Player 2 = {} pontos e Empates = {} \nObrigado por jogar! Feito por: Heitor, Hugo, João e Nathan'.format(placarjogada1, placarjogada2, placarempate))
+            break
+    if modalidade == 2:
+        if continuar == 'n':
+            print('Placar: Player 1 = {} pontos, Bot = {} pontos e Empates = {} \nObrigado por jogar! Feito por: Heitor, Hugo, João e Nathan'.format(placarjogada1, placarjogada2, placarempate))
+            break
+    if modalidade == 3:
+        if continuar == 'n':
+            print('Placar: Bot 1 = {} pontos, Bot 2 = {} pontos e Empates = {} \nObrigado por jogar! Feito por: Heitor, Hugo, João e Nathan'.format(placarjogada1, placarjogada2, placarempate))
+            break
